6d9a7faaf7076cd452d8aadef159dbdf1e8c3bec
peterheinum <peter.heinum@gmail.com>
2021-03-08 20:20:32 +0100

    fixed red.sh script

--- shell/red.sh
+++ shell/red.sh
@@ -1,6 +1,6 @@
 for VARIABLE in 1 2 3 4 5 6 7
 do
   curl -H 'Content-Type: application/json' -X PUT \
-  -d '{"hue": 1000}' \
+  -d '{"hue": 1000, "bri": 10, "sat": 254}' \
   http://192.168.1.8/api/oyjHEB1MhokwXXhauN-fgtdTgKBTAKgPawaySIY6/lights/$VARIABLE/state
 done
